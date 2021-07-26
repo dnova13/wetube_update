ca8cb811bb7ab3526a4dda6df7027d5228e65c7f
dnova13 <dnova13@gmail.com>
2021-07-27 01:48:37 +0900

    2.1. install experess and Get request

--- src/server.js
+++ src/server.js
@@ -5,19 +5,30 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+// 익스프레스에서 라우터 객체 형성 
+const globalRouter = express.Router();
+// req, res 함수 형성(api)
+const handleHome = (req, res) => res.send("Home");
+// router path '/' 지정
+globalRouter.get("/", handleHome);
+
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);

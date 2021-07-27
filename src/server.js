53a59bf1a309a31701885c3a0812bb09559ea707
dnova13 <dnova13@gmail.com>
2021-07-28 02:43:18 +0900

    4.2 Partials

--- src/server.js
+++ src/server.js
@@ -6,14 +6,13 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
-console.log(process.cwd());  /// process.cwd() : 해당 프로젝트 경로 
+console.log(process.cwd());
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug"); // view engin pug 로 셋팅
-app.set("views", process.cwd() + "/src/views"); // view 파일 경로 셋팅
-
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);

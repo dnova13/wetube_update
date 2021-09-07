9a147e6e23d001c77e36b72e0e11ec1109a62f10
dnova13 <dnova13@gmail.com>
2021-09-07 14:23:00 +0900

    8.2 Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,23 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
+
+// 로그인 경우 그 다음 상황으로 진행
+// 아닌 경우 로깅 페이지로 리다렉
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// 로그인 안된 경우 그 다음 상황으로 진행
+// 아닌 경우 홈으로 리다렉
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};

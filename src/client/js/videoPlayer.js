dc216bb8e7abe5ad968ec7f014bac1869bf0e0b7
dnova13 <dnova13@gmail.com>
2021-09-10 08:57:55 +0900

    11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,34 +2,58 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
 
-// 비디오 클릭시 이벤트 설정.
+// 기본 볼륨 값 설정
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+// 비디오 클릭시 이벤트 설정. 코드 간략화
 const handlePlayClick = (e) => {
-  if (video.paused) { // 정지상태면 yes로 반환
-    // playBtn.innerText = "Play"
-    video.play(); // 영상 재생
-  } else {
-    // playBtn.innerText = "Stop"
-    video.pause(); // 영상 멈춤.
-  }
+
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-// 재생 일시 정지일때 이벤트 동작
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-
+// 음소거 이벤트 메소드
 const handleMuteClick = (e) => {
     if (video.muted) {
-      video.muted = false;
+        video.muted = false;
     } else {
-      video.muted = true;
+        video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  };
+    // 음소거 버튼시 볼륨 동작 추가
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
 
-playBtn.addEventListener("click", handlePlayClick); 
-muteBtn.addEventListener("click", handleMute);
+// 볼륨 변화 메소드
+const handleVolumeChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    // 변화된 볼륜 값을 받음.
 
-// 미디어 이벤트 사용.
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+    // 만약 음소거 상태에서 움직일때 
+    // 음소거 상태 해제
+    // 다른 조건이라면 음향 조절 안되게 막을 수 도 잇음.
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+
+    /// 변경된 볼륨값을 기본값으로 재설정.
+    volumeValue = value;
+
+    // 변경된 볼률값을 영상 볼륨에 반영.
+    video.volume = value;
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);

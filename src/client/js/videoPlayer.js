c55f80600fab57a9c2e20530ab8f623c3f35f3d7
dnova13 <dnova13@gmail.com>
2021-09-10 08:42:40 +0900

    11.2 Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,6 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
 
 // 비디오 클릭시 이벤트 설정.
 const handlePlayClick = (e) => {
@@ -14,12 +13,23 @@ const handlePlayClick = (e) => {
     video.pause(); // 영상 멈춤.
   }
 };
+
+// 재생 일시 정지일때 이벤트 동작
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMuteClick = (e) => {
+    if (video.muted) {
+      video.muted = false;
+    } else {
+      video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  };
 
 playBtn.addEventListener("click", handlePlayClick); 
 muteBtn.addEventListener("click", handleMute);
+
+// 미디어 이벤트 사용.
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);

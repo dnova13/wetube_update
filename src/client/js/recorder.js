a543e713ea3602f149eb02485df5bb57137830cc
dnova13 <dnova13@gmail.com>
2021-09-11 14:52:35 +0900

    13.2 Recording Video part 1 : make button ui  and chack recoding state

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,38 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop); /// 스탑 이벤트 제거
+  startBtn.addEventListener("click", handleStart); /// 레코딩 이벤트 재삽입
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop); /// 스탑 이벤트 제거
+    startBtn.addEventListener("click", handleStart); /// 레코딩 이벤트 재삽입
+
+  const recorder = new MediaRecorder(stream);
+
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+
+  console.log(recorder); // 1. state : inactive
+  recorder.start();
+  console.log(recorder); // 2. state : recoding
+
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
@@ -10,5 +40,6 @@ const handleStart = async () => {
   video.play();
 };
 
+init();
+
 startBtn.addEventListener("click", handleStart);
- 

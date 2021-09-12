af934c6e998a40b34e151e789ebd9043ad6ac7cd
dnova13 <dnova13@gmail.com>
2021-09-13 02:59:28 +0900

    16.3 API Route part 1 : make comment api router

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,14 +1,21 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
-
-  console.log("111111")
+  
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: { // form 을 보낼때 이렇게 body 에 묶어서 데이터 보냄
+      text, // text : text -> text 를 간략한 한거.
+    },
+  });
 };
 
-form.addEventListener("submit", handleSubmit);
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}

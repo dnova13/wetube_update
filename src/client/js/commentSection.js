87e74eb9fd47e3160134a1f2247bee0c495c6c1e
dnova13 <dnova13@gmail.com>
2021-09-13 06:16:23 +0900

    16.8 Comment Ids

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,20 +1,22 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
-  const newComment = document.createElement("li"); // 새로운 댓글 태그 생성
-
-  newComment.className = "video__comment";  // 클래스 입력
-  const icon = document.createElement("i"); // 아이콘 태그 생성
+  const newComment = document.createElement("li");  // 새로운 댓글 태그 생성
+  newComment.dataset.id = id; // 댓글 아이디 입력
+  newComment.className = "video__comment"; // 클래스 입력
+  const icon = document.createElement("i"); // 아이콘 태그 샛성
   icon.className = "fas fa-comment"; // 아이콘 클래스 네임 지정
+  const span = document.createElement("span"); // sapn 태그 생성
+  span.innerText = ` ${text}`; // 댓글 텍스트 사빕
+  const span2 = document.createElement("span"); // 아이콘 삽입
+  span2.innerText = "❌";
 
-  const span = document.createElement("span"); // sapn 태그 새성
-  span.innerText = ` ${text}`; // 댓글 텍스트 삽입.
-
-  //  <li> <i></i><span></span> </li> 순으로 <li> 태그에 산입
+   //  <li> <i></i><span></span> </li> 순으로 <li> 태그에 산입
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
 
   /// 댓글 태그 ui에 맨 압쪽에 댓글을 삽입함.
   videoComments.prepend(newComment);
@@ -31,7 +33,8 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -39,10 +42,10 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
 
-  textarea.value = "";
-
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
 };
 

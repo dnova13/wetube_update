662060166069fc0a2577835e147deff404c2b44d
dnova13 <dnova13@gmail.com>
2021-09-13 05:46:17 +0900

    16.7 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,28 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li"); // 새로운 댓글 태그 생성
+
+  newComment.className = "video__comment";  // 클래스 입력
+  const icon = document.createElement("i"); // 아이콘 태그 생성
+  icon.className = "fas fa-comment"; // 아이콘 클래스 네임 지정
+
+  const span = document.createElement("span"); // sapn 태그 새성
+  span.innerText = ` ${text}`; // 댓글 텍스트 삽입.
+
+  //  <li> <i></i><span></span> </li> 순으로 <li> 태그에 산입
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+
+  /// 댓글 태그 ui에 맨 압쪽에 댓글을 삽입함.
+  videoComments.prepend(newComment);
+
+  // 댓글을 맨 뒷쪽에 삽입함.
+  // videoComments.appendChild(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,14 +31,19 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+
   textarea.value = "";
+
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {

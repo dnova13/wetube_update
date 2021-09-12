d361fae835c432d630bc7f9f6018ca2b869a29e8
dnova13 <dnova13@gmail.com>
2021-09-13 03:53:09 +0900

    16.4 API Route part 2 : json strinffy

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,16 +3,18 @@ const form = document.getElementById("commentForm");
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-
+  if (text === "") {
+    return;
+  }
   fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
-    body: { // form 을 보낼때 이렇게 body 에 묶어서 데이터 보냄
-      text, // text : text -> text 를 간략한 한거.
+    headers: {
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify({ text }), // 객체를 string 화
   });
 };
 
